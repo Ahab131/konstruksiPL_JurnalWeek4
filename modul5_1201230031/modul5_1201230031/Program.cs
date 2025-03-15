840d49c5b6867b70428b9f55b5835fee5817520b
Ahab131 <hakimalbaihaqy100@gmail.com>
2025-03-15 10:42:30 +0700

    menambahkan class SimpleDataBase dengan method PrintAllData

---
+++ modul5_1201230031/modul5_1201230031/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul5_1201230031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buat instance dari SimpleDataBase dengan tipe data sesuai NIM
+            SimpleDataBase<int> database = new SimpleDataBase<int>();
+
+            // Contoh NIM: 12345678 (digit terakhir adalah 8)
+            // Berdasarkan aturan, NIM berakhiran 6, 7, atau 8 menggunakan tipe data int
+            int nimAkhiran = 8; // Ganti dengan digit terakhir NIM Anda
+
+            // Tambahkan tiga data sesuai 2-digit NIM
+            database.AddNewData(12); // Contoh input
+            database.AddNewData(34);
+            database.AddNewData(56);
+
+            // Cetak semua data
+            database.PrintAllData();
+        }
+    }
+}
---
+++ modul5_1201230031/modul5_1201230031/SimpleDataBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1201230031
+{
+    class SimpleDataBase<T>
+    {
+        // Property untuk menyimpan data dan waktu input
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        // Konstruktor
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        // Method untuk menambahkan data baru
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.UtcNow);
+        }
+
+        // Method untuk mencetak semua data
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+            }
+        }
+    }
+}
